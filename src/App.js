16cc62979835a15f1fbe36b0f53b81c42d2740da
bb <bbna@bbs-MacBook-Air.local>
2023-01-09 19:06:56 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-//ứng dụng đếm số
+import { useState } from "react";//ứng dụng đếm số
+
+//vd 1 tăng giá trị lên 1 đơn vị, 
+// function App() {
+//   const [ giaTriBanDau, hamTangGiaTri ] = useState(1)
+//   const handleIncrease = () => {
+//     hamTangGiaTri(giaTriBanDau + 1)
+//   }
+//   return (
+//     <div className="App" style={{padding: 100}}>
+//       <h2>{giaTriBanDau}</h2>
+//       <button onClick={handleIncrease}>Click me</button>
+//     </div>
+//   );
+// }
+
+//tăng lên 3 đơn vị, tham số return của hàm setCouter
+// function App() {
+//   const [counter, setCounter] = useState(1);
+//   const handleIncrease = () => {
+//     setCounter(pre => pre +3);
+//   }
+//   return (
+//     <div className="App" style={{ padding: 100 }}>
+//       <h2>{counter}</h2>
+//       <button onClick={handleIncrease}>Click Me</button>
+//     </div>
+//   )
+// }
+
+//add thêm thuộc tính cho mảng.
 
 function App() {
+  const [info, updateInfo] = useState({ name: "Ho Viet Dung", tuoi: 32, DiaChi: "japan" })
+  const handleUpdate = () => {
+    updateInfo({
+      ...info, bio: "Yeu Panda love love"
+    })
+  }
   return (
     <div className="App">
-      
+      <h2>Thông tin user.</h2>
+      <p>{JSON.stringify(info)}</p>
+      <button onClick={handleUpdate}>Click me to add info</button>
     </div>
-  );
+  )
 }
-
 export default App;
