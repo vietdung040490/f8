36bd2b4f0c87fa1e44f1dc3ee24e7307fd886a7d
bb <bbna@bbs-MacBook-Air.local>
2023-01-10 19:47:48 +0900

    update

--- src/App.js
+++ src/App.js
@@ -29,20 +29,52 @@ import { useState } from "react";//ứng dụng đếm số
 // }
 
 //add thêm thuộc tính cho mảng.
+// function App() {
+//   const [info, updateInfo] = useState({ name: "Ho Viet Dung", tuoi: 32, DiaChi: "japan" })
+//   const handleUpdate = () => {
+//     updateInfo({
+//       ...info, bio: "Yeu Panda love love"
+//     })
+//   }
+//   return (
+//     <div className="App">
+//       <h2>Thông tin user.</h2>
+//       <p>{JSON.stringify(info)}</p>
+//       <button onClick={handleUpdate}>Click me to add info</button>
+//     </div>
+//   )
+// }
 
+// Nhận phần thưởng.
+// const gifts = ['quà 1', 'quà ２', 'quà 3', 'quà 4', 'quà 5'];
+// const quaDaNhan = [];
+// function App() {
+//     const [gift, setGift] = useState();
+//     function handleRandomGift(){
+//         let random = Math.floor(Math.random() * (gifts.length));
+//         setGift(gifts[random])
+//         quaDaNhan.push(gift);
+//     }
+//     return (
+//         <div className="App" style={{padding: 100}}>
+//             <h2>{gift || 'Chưa có quà'}</h2>
+//             <button onClick={handleRandomGift}>Click để nhận quà</button>
+//             <p>qua da nhan: {JSON.stringify(quaDaNhan)}</p>
+//         </div>
+//     )
+// }
+
+//two ways binding
+// <input />
 function App() {
-  const [info, updateInfo] = useState({ name: "Ho Viet Dung", tuoi: 32, DiaChi: "japan" })
-  const handleUpdate = () => {
-    updateInfo({
-      ...info, bio: "Yeu Panda love love"
-    })
-  }
-  return (
-    <div className="App">
-      <h2>Thông tin user.</h2>
-      <p>{JSON.stringify(info)}</p>
-      <button onClick={handleUpdate}>Click me to add info</button>
-    </div>
-  )
+    const [value, setValue] = useState()
+    console.log(value)
+    
+    return (
+        <div className="App" style={{ padding: 100 }}>
+            <h2>bla bla input</h2>
+            <input onChange={(e) => setValue(e.target.value)} />
+        </div>
+    )
 }
 export default App;
